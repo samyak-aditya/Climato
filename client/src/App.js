b69f2510792dd14ee0b03d48ff93d072b49fe6f2
SudhanshuSingh6 <sudhanshus026@gmail.com>
2024-03-15 16:22:19 +0530

    added recyler login and signout form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login.js"
+import Login from "./pages/user/Login.js"
+import Signup from "./pages/user/Signup.js"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />}>
-        </Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>I
       </Routes>
     </BrowserRouter>
   );
